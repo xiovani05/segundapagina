f2678b6b2b42444c06551171d5d24ba446d5d015
xiovani05 <yovanivazquezguzman738@gmail.com>
2026-06-27 13:52:31 -0600

    Update password for valid user

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Base de datos simulada del estudiante
 const USUARIO_VALIDO = {
     matricula: "2025452046",
-    password: "alumno123"
+    password: "123"
 };
 
 // Tus 7 materias oficiales del TESCHI
